b53a0d79d0af3e1701ad8f4efc84896ddbbf39ac
Zengo-Master <8435740@gmail.com>
2021-01-08 22:58:44 +0900

    編集と削除

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,5 @@
 class VideosController < ApplicationController
+  before_action :set_video, only: [:edit, :show, :destroy, :update]
 
   def index
     @videos = Video.all.order('created_at DESC')
@@ -25,7 +26,25 @@ class VideosController < ApplicationController
   end
 
   def show
-    @video = Video.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def destroy
+    if @video.destroy
+      redirect_to root_path
+    else
+      redirect_to video_path(@video.id)
+    end
+  end
+
+  def update
+    if @video.update(video_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -33,4 +52,9 @@ class VideosController < ApplicationController
   def video_params
     params.require(:video).permit(:video, :title, :image)
   end
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
 end
